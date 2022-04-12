f4f8654109b9aeeae1b98076ef29ec9912c96aaa
Ryan Allred <ryanallredxyz@gmail.com>
2022-04-12 14:36:34 -0600

    Still broken

--- service1/index.js
+++ service1/index.js
@@ -1,7 +1,8 @@
 const app = require("express")();
+const moment = require("moment");
 
 app.use((_, res) => {
-  res.send("Hello from service 1");
+  res.send(`Hello from service 1 ${moment().format("l")}`);
 });
 
 app.listen(4050);
--- service2/index.js
+++ service2/index.js
@@ -1,7 +1,8 @@
 const app = require("express")();
+const { faker } = require("@faker-js/faker");
 
 app.use((_, res) => {
-  res.send("Hello from service 2");
+  res.send(`Hello from service 2 (${faker.lorem.words(5)})`);
 });
 
 app.listen(4050);
